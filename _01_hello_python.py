a0b9a94e8b1842f8157ef9fbae980e8cffc4f159
hudung0831 <hudung0831@gmail.com>
2026-05-29 16:17:43 +0900

    .gitignore 작성

---
+++ _01_hello_python.py
@@ -0,0 +1,20 @@
+# 주석(Comment) : 코드 설명 작성
+# - 이 주석은 파이썬 인터프리터가 해석하지 않고 넘어감(무시)
+
+# 한 줄 주석 (windows : Ctrl + /, Mac : Cmd + /)
+print("Hello Python")
+print(123)
+print(123.456)
+
+# 여러 줄 문자열 작성
+print("""
+안녕하세요 
+오늘 볼 수 있나요?
+""")
+
+# 여러 줄 주석
+print("""
+always I like jr
+everyday
+""")
+
