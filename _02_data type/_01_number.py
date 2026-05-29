fc9033190c8e3575df7b518dc33dea54222ab067
hudung0831 <hudung0831@gmail.com>
2026-05-29 17:49:32 +0900

    number and bool

---
+++ _02_data type/_01_number.py
@@ -0,0 +1,17 @@
+# number (숫자형) : 정수, 실수, 복소수
+# type (변수명 | 값) 함수 : 변수 또는 값의 타입을 확인하는 내장 함수
+# 정수 (int)
+
+n = 123
+print(n, type(n))
+
+price = 10_000_000_000; # 정수 자릿수 구분
+print(price, type(price))
+
+# 정수(int) 최댓값
+import sys
+print(sys.maxsize)
+
+# 복소수 (complex)
+
+print (3**2)
