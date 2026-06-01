f88ea0c19d7570ab630686a7e8e42c00de4ff661
hudung0831 <hudung0831@gmail.com>
2026-06-01 18:24:05 +0900

    2026 06 01 과제 수행

---
+++ _homework_01.py
@@ -0,0 +1,86 @@
+#1 변수
+#1_1
+name = '이서영'
+age = 23
+#1_2
+height = 164
+dream = 'make a good effect to the world'
+#1_3
+city = 'Seoul'
+my_city = city ###
+print(city) ###
+#1_4
+apple_count = 10
+orange_count = 20
+print(apple_count)
+print(orange_count)
+#1_5
+first_name = 'Seo'
+last_name = 'Lee'
+full_name = first_name + last_name
+print(full_name)
+#2_1
+num1 = 10
+num2 = 5
+sum_result = num1 + num2
+print(sum_result)
+#2_2
+x = 7.5
+y = 2
+result = x/y
+print(result)
+#2_3
+is_student = True
+print("is_student = ", is_student)
+#2_4
+num1 = 15
+num2 = 4
+print (num1/num2)
+print (num1//num2)
+#2_5
+greeting_message = "Hello"
+greeting_target = "World"
+print(greeting_message + greeting_target)
+#2_6
+base = 2
+exponent = 5 ###
+#2_7
+number = 3
+even = number// 2 != 0
+#2_8
+age = 23 ###
+city = 'Seoul'
+print(f' "나는 age살이고, city에 살고 있습니다.", age, city')
+
+#3_01 ###
+#3_02 (2)
+#3_1
+fruits = ['apple', 'banana', 'cherry']
+print(fruits[::2])
+#3_2
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))
+#3_3
+animals = ['cat', 'dog', 'bird']
+###
+#3_4
+numbers = [3, 6, 9]
+###
+#3_5
+lst = 'Python-is-fun'
+### 인덱스로 끊어야 하나? 슬라이싱?
+#3_6
+my_tuple = {10, 20, 30}
+print(my_tuple[1])
+#3_7
+colors = ['red', 'blue', 'green']
+###
+#3_8
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+nums/2 = 0
+#3_9
+words = ['Life, 'is', 'too', 'short]
+print(words.plus?) ###
+#3_10
+lst2 = [1, 2, 3, 4, 5]
+###
